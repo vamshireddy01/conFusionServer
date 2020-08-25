3ab31d5e549686178381318f3b5e6369e8d440b0
vamshireddy01 <m.vamshikrishna8@gmail.com>
2020-08-25 17:00:05 +0530

    Passport

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
@@ -45,27 +48,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
+  if(!req.user) {
     var err = new Error('You are not authenticated!');   
     err.status = 403;
     return next(err);
   }
 
   else {
-    if (req.session.user == 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');      
-      err.status = 403;
-      return next(err); 
-    }
+    next();
   }
 }
 
