90eaa309c0bd93baa2a790e178a5ea45f3445b15
vamshireddy01 <m.vamshikrishna8@gmail.com>
2020-08-26 13:39:34 +0530

    Passport JWT

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const Dishes = require('./models/dishes');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 
-const url = 'mongodb://localhost:27017/conFusion';
+var config = require('./config');
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -40,35 +41,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if(!req.user) {
-    var err = new Error('You are not authenticated!');   
-    err.status = 403;
-    return next(err);
-  }
-
-  else {
-    next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
